9b3e7846af390b1679eda760868fcdbff4398cac
NetSky-Aero <154016588+NetSky-Aero@users.noreply.github.com>
2023-12-21 21:51:54 +0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("    Hello word!\n");
+    
+    printf("                Let’s\n");
+    printf("                    go\n");
+    printf("                       to walk\n");
+    
+    printf("                                     *\n");
+    printf("                                    ***\n");
+    printf("                                   *****\n");
+    printf("                                  *******\n");
+    printf("                                   HH HH\n");
+    printf("                                   ZZZZZ\n");
+    
+    return 0;
+}
+

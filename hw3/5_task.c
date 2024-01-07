a21b28231bb00960ed3dc8b0ebe84452c460f60a
NetSky-Aero <154016588+NetSky-Aero@users.noreply.github.com>
2024-01-07 13:55:58 +0300

    Add files via upload

---
+++ hw3/5_task.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    printf("Ввести трехзначное целое положительное число: ");
+    if (scanf("%d", &a ) != 1 || ! a)
+        return 1;
+    for ( b = 1; a; a /= 10 )
+        b *= a % 10;
+    printf("Произведение: %d", b);
+    return 0;
+}
+

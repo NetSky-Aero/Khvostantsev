a65d292358e8f778ec86d9d652260c725cb4ca03
NetSky-Aero <154016588+NetSky-Aero@users.noreply.github.com>
2024-01-07 10:33:50 +0300

    Update 3_task.c

--- hw3/3_task.c
+++ hw3/3_task.c
@@ -5,7 +5,6 @@ int main(void)
     int a,b,c;
     printf("Два целых числа через пробел:\n");
     scanf("%d%d", &a, &b);
-    //printf("%d-%d=%d", a, b, a-b);
     c = a-b;
     printf("%d", c);
     return 0;

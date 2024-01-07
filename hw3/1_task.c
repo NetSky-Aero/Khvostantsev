05ccdf75c0082968311e9a0c1db5b7d8c94b6e04
NetSky-Aero <154016588+NetSky-Aero@users.noreply.github.com>
2024-01-07 11:01:40 +0300

    Update 1_task.c

--- hw3/1_task.c
+++ hw3/1_task.c
@@ -6,7 +6,7 @@ int main(void)
     printf("Три целых числа через пробел: ");
     scanf("%d%d%d", &a, &b, &c);
     x=a+b+c;
-    printf("%d", x);
+    printf("%d", x); // можно было по варианту "printf ("%d+%d+%d=%d", a, b, с, a+b+с )", но захотел сделать так
     return 0; 
 }
 
